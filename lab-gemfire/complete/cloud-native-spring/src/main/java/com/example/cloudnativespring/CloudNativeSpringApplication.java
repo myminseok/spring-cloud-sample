63f7015705f711f7c3be5f4fa830f02632772859
MinSeok Kim <myminseok@gmail.com>
2021-06-07 18:00:38 +0900

    test

--- lab-gemfire/complete/cloud-native-spring/src/main/java/com/example/cloudnativespring/CloudNativeSpringApplication.java
+++ lab-gemfire/complete/cloud-native-spring/src/main/java/com/example/cloudnativespring/CloudNativeSpringApplication.java
@@ -3,12 +3,10 @@ package com.example.cloudnativespring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.geode.config.annotation.EnableClusterAware;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableClusterAware
 public class CloudNativeSpringApplication {
 
--- lab-gemfire/complete/cloud-native-spring/src/main/java/com/example/cloudnativespring/MyController.java
+++ lab-gemfire/complete/cloud-native-spring/src/main/java/com/example/cloudnativespring/MyController.java
@@ -2,6 +2,13 @@ package com.example.cloudnativespring;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class MyController {
@@ -39,8 +46,12 @@ public class MyController {
 		}
 
 		System.out.println("note:"+note);
+
 		notes.add(note);
 		request.getSession().setAttribute("NOTES", notes);
+
+		List<String> notes = (List<String>) request.getSession().getAttribute("NOTES");
+		System.out.println("notes.size:"+notes.size());
 	}
 
 	@PostMapping("/invalidateSession")
